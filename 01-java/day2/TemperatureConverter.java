73544b3b385d541a803555fddc7dab0c2ced2dff
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 14:02:54 +0530

    TemperatureConverter program

---
+++ 01-java/day2/TemperatureConverter.java
@@ -0,0 +1,7 @@
+public class TemperatureConverter{
+    public static void main(String[] args) {
+        double celcius = 19.9;
+        double fahrenheit = (celcius * 9/5) + 32;
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit + "F");
+    }
+}
