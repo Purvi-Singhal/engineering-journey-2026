a681add8989166bedd6c781c6dfb14b5d5c928e1
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 19:48:45 +0530

    docs(02-operators): add problem statement and compound assignment trace to q3

--- 01-java/02-operators/questions/q3.java
+++ 01-java/02-operators/questions/q3.java
@@ -1,13 +1,37 @@
-package chapter2.questions;
+package questions;
 
+/**
+ * Question 3: Compound Assignment Operators Tracing
+ * 
+ * Problem Statement:
+ * What will be the output of the following Java program?
+ * 
+ *   int x, y, z;
+ *   x = y = z = 2;
+ *   x += y;
+ *   y -= z;
+ *   z /= (x + y);
+ *   System.out.println(x + " " + y + " " + z);
+ * 
+ * Step-by-Step Evaluation:
+ * 1. Chained assignment: x = 2, y = 2, z = 2.
+ * 2. x += y  -> x = x + y -> x = 2 + 2 = 4.
+ * 3. y -= z  -> y = y - z -> y = 2 - 2 = 0.
+ * 4. z /= (x + y) -> z = z / (4 + 0) -> z = 2 / 4.
+ *    - In integer division, 2 / 4 evaluates to 0 (fractional part is truncated).
+ *    - Therefore, z = 0.
+ * 
+ * Expected Output:
+ * 4 0 0
+ */
 public class q3 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int x, y, z;
-        x=y=z=2;
+        x = y = z = 2;
         x += y;
         y -= z;
-        z /= (x+y); //it will give 0 because 2/4 = 0.5 and ans will be in int so 0
+        z /= (x + y);
 
-        System.out.println(x + " " + y + " " +z);
+        System.out.println(x + " " + y + " " + z);
     }
 }
