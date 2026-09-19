f4df7219a96dc0934f8a18565b50ae4e5ed7149f
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 19:47:23 +0530

    docs(01-basics): add problem statement and documentation to q2 (area of a square)

--- 01-java/01-basics/questions/q2.java
+++ 01-java/01-basics/questions/q2.java
@@ -1,9 +1,25 @@
 package questions;
 
+/**
+ * Question 2: Area of a Square
+ * 
+ * Problem Statement:
+ * In a program, input the side of a square. You have to output the area of the
+ * square.
+ * 
+ * Formula:
+ * Area = side * side
+ * 
+ * Sample Input:
+ * side = 23
+ * 
+ * Expected Output:
+ * 529
+ */
 public class q2 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int side = 23;
-        int area = side*side;
-        System.out.println(area);
+        int area = side * side;
+        System.out.println("Area of Square: " + area);
     }
 }
