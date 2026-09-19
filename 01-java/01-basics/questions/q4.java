233ee0165a84d2f9d2fe59217ac35276254b60bf
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 19:47:50 +0530

    docs(01-basics): add problem statement and documentation to q4 (type promotion in expressions)

--- 01-java/01-basics/questions/q4.java
+++ 01-java/01-basics/questions/q4.java
@@ -1,17 +1,43 @@
 package questions;
 
+/**
+ * Question 4: Type Promotion & Identifier Rules
+ * 
+ * Problem Statement:
+ * What will be the type of result in the following Java code?
+ *   byte b = 4;
+ *   char c = 'a';
+ *   short s = 512;
+ *   int i = 1000;
+ *   float f = 5.67f;
+ *   double d = 0.1234;
+ *   result = (f * b) + (i / c) - (d * s);
+ * 
+ * Concept:
+ * - (f * b) -> float * byte => float
+ * - (i / c) -> int / char => int
+ * - (d * s) -> double * short => double
+ * - float + int - double => double
+ * 
+ * The entire expression is promoted to the largest data type involved, which is `double`.
+ * 
+ * Also tests identifier rules:
+ * - Is `int $ = 24;` valid? Yes, '$' and '_' are allowed as identifier characters in Java.
+ */
 public class q4 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         byte b = 4;
         char c = 'a';
         short s = 512;
         int i = 1000;
         float f = 5.67f;
         double d = 0.1234;
-        double result = (f * b) + (i / c) - (d * s);
-        System.out.println(result);
 
-        int $ = 24; //it will not give any error because $ is a valid identifier in java but it is not recommended to use it as it can create confusion and make the code less readable.
-        System.out.println($);
+        double result = (f * b) + (i / c) - (d * s);
+        System.out.println("Result of expression: " + result);
+
+        // Valid identifier demonstration
+        int $ = 24;
+        System.out.println("Value of identifier $: " + $);
     }
 }
