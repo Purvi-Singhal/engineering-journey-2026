3fda7b9587906c83238442c4ef2f86d422994dcb
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 19:48:32 +0530

    docs(02-operators): add problem statement and logical evaluation trace to q2

--- 01-java/02-operators/questions/q2.java
+++ 01-java/02-operators/questions/q2.java
@@ -1,17 +1,48 @@
-package chapter2.questions;
+package questions;
 
+/**
+ * Question 2: Logical Operators & Short-Circuit Evaluation
+ * 
+ * Problem Statement:
+ * What will be the output of the following code?
+ * 
+ *   int x = 200, y = 50, z = 100;
+ *   if(x > y && y > z) {
+ *       System.out.println("Hello");
+ *   }
+ *   if(z > y && z < x) {
+ *       System.out.println("Java");
+ *   }
+ *   if((y + 200) < x && (y = 150) < z) {
+ *       System.out.println("Hello Java");
+ *   }
+ * 
+ * Step-by-Step Evaluation:
+ * 1. (x > y && y > z) -> (200 > 50 && 50 > 100) -> (true && false) => false
+ *    - "Hello" is NOT printed.
+ * 
+ * 2. (z > y && z < x) -> (100 > 50 && 100 < 200) -> (true && true) => true
+ *    - "Java" IS printed!
+ * 
+ * 3. ((y + 200) < x && ...) -> ((50 + 200) < 200) -> (250 < 200) => false
+ *    - Because the left condition of '&&' is false, Java short-circuits and ignores the right side.
+ *    - "Hello Java" is NOT printed.
+ * 
+ * Expected Output:
+ * Java
+ */
 public class q2 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int x = 200, y = 50, z = 100;
-        if(x > y && y > z){
+
+        if (x > y && y > z) {
             System.out.println("Hello");
         }
-        if(z>y && z<x){
+        if (z > y && z < x) {
             System.out.println("Java");
         }
-
-        if((y+200)<x && (y=150)<z){
-            System.out.println("hell java");
+        if ((y + 200) < x && (y = 150) < z) {
+            System.out.println("Hello Java");
         }
     }
 }
