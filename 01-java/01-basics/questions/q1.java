818b4bf3ee9123e8dee384b40c49bb3cfd68a854
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-19 19:47:11 +0530

    docs(01-basics): add problem statement and documentation to q1 (average of 3 numbers)

--- 01-java/01-basics/questions/q1.java
+++ 01-java/01-basics/questions/q1.java
@@ -1,13 +1,27 @@
 package questions;
 
-
+/**
+ * Question 1: Average of 3 Numbers
+ * 
+ * Problem Statement:
+ * In a program, input 3 numbers: A, B, and C. You have to output the average of
+ * these 3 numbers.
+ * 
+ * Formula:
+ * Average = (A + B + C) / 3
+ * 
+ * Sample Input:
+ * A = 25, B = 30, C = 35
+ * 
+ * Expected Output:
+ * 30.0
+ */
 public class q1 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int a = 25;
         int b = 30;
         int c = 35;
-        float avg = (a+b+c)/3;
-        System.out.println(avg);
-        
-}
+        float avg = (float)(a + b + c) / 3;
+        System.out.println("Average: " + avg);
+    }
 }
