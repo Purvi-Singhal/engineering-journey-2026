2cbde67d10290cc6bd28e3f3273f7c6ed6db6ecd
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-20 21:13:48 +0530

    feat(04-conditionals): add ifelse.java demonstrating if-else and else-if ladder

---
+++ 01-java/04-conditionals/ifelse.java
@@ -0,0 +1,27 @@
+/* if(condition){
+        true --> execute 
+        } 
+    else{
+        false --> execute
+        }
+*/
+
+// to check if a person is adult, minor or teenager
+import java.util.Scanner;
+class ifelse{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();  //age input from user
+        if(age>=18){
+            System.out.println("Adult");
+        }
+        else if(age>=13 && age<18){
+            System.out.println("Teenager");
+        }
+        else{
+            System.out.println("Minor");
+        }
+        sc.close();
+    }
+}
