a3333fabc3e4b1e7413d526f1ec48d7be70c6944
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-20 10:29:04 +0530

    Unary Operators

--- 01-java/02-operators/arithmetic.java
+++ 01-java/02-operators/arithmetic.java
@@ -1,4 +1,4 @@
-package chapter2;
+
 
 public class arithmetic {
     public static void main(String args[]){
@@ -13,26 +13,29 @@ public class arithmetic {
         System.out.println("mod = " + (a%b));
  
         //Unary operators
+        //Pre Increment 
         int x = 10;
         int y = ++x;
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-
+        System.out.println("x = " + x);     // x = 11
+        System.out.println("y = " + y);     // y = 11
+ 
+        //Post Increment
         int p = 10;
         int q = p++;
-        System.out.println("p = " + p);
-        System.out.println("q = " + q);
-        System.out.println("p = " + p);
+        System.out.println("p = " + p);     // p = 11
+        System.out.println("q = " + q);     // q = 10
+        System.out.println("p = " + p);     // p = 11
 
+        //pre decrement
         int u = 10;
         int v = --u;
-        System.out.println("u = " + u);
-        System.out.println("v = " + v);
+        System.out.println("u = " + u);    // u = 9
+        System.out.println("v = " + v);    // v = 9
 
         int c = 10;
         int d = c--;
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
+        System.out.println("c = " + c);    // c = 9
+        System.out.println("d = " + d);    // d = 10
 
 
 
