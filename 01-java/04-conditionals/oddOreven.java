12a16acf5162afbc2f91958d6799bba986a94cb2
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-20 21:21:07 +0530

    docs(04-conditionals): add explanatory notes on modulo operator and parity in oddOreven.java

--- 01-java/04-conditionals/oddOreven.java
+++ 01-java/04-conditionals/oddOreven.java
@@ -1,4 +1,6 @@
 // Print if a number is odd or even
+//if a number gives remainder 0 after ddividing by 2 is said to be even otherwise odd.
+//% is the modulo operator which gives remainder of a division.
 import java.util.Scanner;
 public class oddOreven{
     public static void main(String[] args){
