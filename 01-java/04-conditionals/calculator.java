b6710df06595fd54a5d60adc332ce730c3569725
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-22 08:41:58 +0530

    refactor(04-conditionals): update calculator.java formatting and messages

--- 01-java/04-conditionals/calculator.java
+++ 01-java/04-conditionals/calculator.java
@@ -1,48 +1,54 @@
 // Simple Calculator using switch statement
 
 import java.util.Scanner;
+class c{
+    public static void main(String[] args){
 
-class calculator {
-    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number (a): ");
+        System.out.print("Enter first number : ");
         double a = sc.nextDouble();
 
-        System.out.print("Enter second number (b): ");
+        System.out.print("Enter second number : ");
         double b = sc.nextDouble();
 
-        System.out.print("Enter operator (+, -, *, /, %): ");
-        char operator = sc.next().charAt(0);
+        System.out.print("Enter the operation you want to perform(+, -, *, /, %) : ");
+        char op = sc.next().charAt(0);
 
-        switch (operator) {
+        switch(op){
             case '+':
-                System.out.println("Result: " + (a + b));
+                System.out.println("Addition of "+a+" and "+b+" is: "+(a+b));
                 break;
-            case '-':
-                System.out.println("Result: " + (a - b));
+
+            case '-' :
+                System.out.println("Subtraction of "+a+" and "+b+" is: "+(a-b));
                 break;
+            
             case '*':
-                System.out.println("Result: " + (a * b));
+                System.out.println("Multiplication of "+a+" and "+b+" is: "+(a*b));
                 break;
+
             case '/':
-                if (b != 0) {
-                    System.out.println("Result: " + (a / b));
-                } else {
+                if(b!=0){
+                    System.out.println("Division of "+a+" and "+b+" is: "+(a/b));
+                }
+                else{
                     System.out.println("Error: Division by zero is undefined.");
                 }
                 break;
+            
             case '%':
-                if (b != 0) {
-                    System.out.println("Result: " + (a % b));
-                } else {
-                    System.out.println("Error: Modulo by zero is undefined.");
+                if(b!=0){
+                    System.out.println("Remainder of "+a+" and "+b+" is: "+(a%b));
+                }
+                else{
+                    System.out.println("Error: Modulo by zero is not defined.");
                 }
                 break;
-            default:
-                System.out.println("Invalid operator.");
-        }
 
+            default:
+                System.out.println("operation you choose is not valid.");
+        }
         sc.close();
     }
 }
