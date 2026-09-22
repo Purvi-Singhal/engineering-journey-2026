6e9690acb6f0db2751366814cca665135a906964
Purvi Singhal <purvisinghal86301@gmail.com>
2026-09-22 22:54:03 +0530

    feat(05-loops): add while loop implementation

---
+++ 01-java/05-loops/whileLoop.java
@@ -0,0 +1,21 @@
+// while loop 
+// use when we have to do some task repeatedly.
+
+//Synatx: 
+// while(condition){
+    //code
+//  }
+
+//Print numbers from 1 to 10 using while loop
+
+class whileLoop{
+    public static void main(String[] args){
+
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("1 to 10 number printed.");
+    }
+}
